28db9d4328c0c9672e85e6edaef84fb0299968b6
t-nabe07 <watanabe.t.job@gmail.com>
2022-04-02 09:27:22 +0900

    java10

--- Java_practice/src/exception/Exception04.java
+++ Java_practice/src/exception/Exception04.java
@@ -1,5 +1,26 @@
 package exception;
 
-public class Exception04 {
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 
+public class Exception04 {
+	public static void main(String[] args) {
+		try {
+			// readFileメソッド呼び出し
+			Exception04.readFile("exception.txt");
+		// 呼び出し元も例外処理を記述しないとコンパイルエラーになる
+		} catch (FileNotFoundException e) {
+			// 例外処理が実行される
+			System.out.println("FileNotFoundException例外が発生");
+		}
+		System.out.println("プログラムが終了");
+	}
+
+	// 検査例外のFileNotFoundException例外をスロー
+	public static void readFile(String fileName) throws FileNotFoundException {
+		System.out.println("ファイルの読み込み開始");
+		// ファイルが存在しないため例外が発生
+		FileReader fr = new FileReader(fileName);
+		System.out.println(fileName + "の読み込み完了");
+	}
 }
